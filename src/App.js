d2913e01ededf4f5444a199bdd3ba2c2df0054fe
lililliililil <cometoijpark@gmail.com>
2024-06-30 22:02:45 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* esLint-disable */
+
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,33 @@
 // Blog.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Blog.css';
 
 const Blog = () => {
+
+    let [postname, changepostname] = useState(['test1', 'test2', 'test3',' test4'])
+    let [day, daychange] = useState(['6|30', '6|30', '6|29', '6|28'])
+
     return (
         <div className='Blog'>
-            <h1>name Page</h1>
+            <div className='post'>
+                <h3> { postname[1] }  <span>♥</span>  </h3>
+                <p> { day[1] } </p>
+            </div>
+            <div className='post'>
+                <h3> { postname[2] } </h3>
+                <p> { day[2] } </p>
+                <hr />
+            </div>
+            <div className='post'>
+                <h3> { postname[3] } </h3>
+                <p> { day[3] } </p>
+                <hr />
+            </div>
+            <div className='post'>
+                <h3> { postname[4] } </h3>
+                <p> { day[4] } </p>
+                <hr />
+            </div>
         </div>
     );
 };
